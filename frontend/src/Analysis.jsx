b4a3533b0999d61d3e7cffaf32c87b1e32bbd018
smit-mist <smit3355mistry@gmail.com>
2022-08-17 17:18:57 +0530

    Displaying tree.

--- frontend/src/Analysis.jsx
+++ frontend/src/Analysis.jsx
@@ -1,19 +1,57 @@
 import { useState, useEffect } from "react";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
-import {GameTree} from "./GameTree";
+import { GameTree } from "./GameTree";
+import Tree from "react-d3-tree";
+
+const orgChart = {
+  name: "CEO",
+  children: [
+    {
+      name: "Manager",
+      attributes: {
+        department: "Production",
+      },
+      children: [
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Fabrication",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+        {
+          name: "Foreman",
+          attributes: {
+            department: "Assembly",
+          },
+          children: [
+            {
+              name: "Worker",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
 const Analysis = () => {
   const [game, setGame] = useState(new Chess());
   const tree = new GameTree();
-  tree.addNode({move:""});
-  tree.addNode({move:"e4"}, 1);
-  tree.addNode({move:"d4"}, 1);
-  tree.addNode({move:"c5"}, 2);
+  tree.addNode({ move: "" });
+  
   console.log(tree.root);
   return (
-    <div>Analysis</div>
-  )
-}
+    <div id="treeWrapper" style={{ width: "500em", height: "500em" }}>
+      {" "}
+      <Tree data={tree.root} />
+    </div>
+  );
+};
 
-export default Analysis
+export default Analysis;
