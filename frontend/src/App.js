b7fca17919790346b6d443a538797a76ffdaaacd
smit-mist <smit3355mistry@gmail.com>
2022-08-18 20:06:28 +0530

    app bar added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import Analysis from "./Analysis";
 import PlayRandomMoveEngine from "./Test";
-
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-       {/* <PlayRandomMoveEngine/> */}
-       <Analysis/>
+      <Router>
+      <ResponsiveAppBar />
+
+        <Routes>
+          <Route path="/" element={<h1>This is home</h1>} />
+          <Route path="/analysis" element={<Analysis />} />
+          <Route path="/tv" element={<h1>This is Live TV</h1>} />
+          <Route path="/news" element={<h1>This is News</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
