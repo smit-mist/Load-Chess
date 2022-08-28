abaa3aa636b35bb0272ccbd2ebea6c5375c80e80
smit-mist <smit3355mistry@gmail.com>
2022-08-29 00:19:02 +0530

    small changes.

--- frontend/src/components/RoundGames.jsx
+++ frontend/src/components/RoundGames.jsx
@@ -9,17 +9,38 @@ const RoundGames = (props) => {
   const roundId = props.id;
   console.log("New round is created", roundId);
   const [loading, setLoading] = useState(true);
-  const [currentRound, setCurrentRound] = useState({});
+  const [roundGames, setRoundGames] = useState([]);
   const getCurrentRound = async () => {
     if (isCalled) {
-        console.log("Repeated");
+      console.log("Repeated");
       return;
     }
     isCalled = true;
     const apiUrl = `https://lichess.org/api/broadcast/round/${roundId}.pgn`;
     const response = await axios.get(apiUrl);
     console.log("get round", response.data, typeof response.data);
-    setCurrentRound(response.data);
+    let allGames = [];
+    let pgnString = response.data;
+    let curr = "";
+    for (let i = 0; i < pgnString.length; i++) {
+      if (i !==0&&
+        i + 2 < pgnString.length &&
+        pgnString[i] == "[" &&
+        pgnString[i + 1] == "E" &&
+        pgnString[i + 2] == "v"
+      ) {
+        allGames.push(curr);
+        curr = "";
+        curr += pgnString[i];
+      } else {
+        curr += pgnString[i];
+      }
+      if (i == pgnString.length - 1) {
+        allGames.push(curr);
+        curr = "";
+      }
+    }
+    setRoundGames(allGames);
     setLoading(false);
   };
   useEffect(() => {
@@ -28,9 +49,10 @@ const RoundGames = (props) => {
   if (loading) {
     return <Loader />;
   }
+
   return (
     <div>
-      <ViewFEN pgn={currentRound} />
+      <ViewFEN allGames={roundGames} />
     </div>
   );
 };
--- frontend/src/components/ViewFEN.jsx
+++ frontend/src/components/ViewFEN.jsx
@@ -5,41 +5,13 @@ import { useEffect, useState } from "react";
 import Loader from "./Loader";
 
 const ViewFEN = (props) => {
-  const { pgn } = props;
+  const { allGames } = props;
   const [game, setGame] = useState(new Chess());
   const [loading, setLoading] = useState(true);
   const loadGame = () => {
-    let smit = "";
-    let ignore = false;
-    for (let i = 0; i < pgn.length; i++) {
-      if (ignore) {
-        if (pgn[i] == "}") {
-          ignore = false;
-        } else {
-          continue;
-        }
-      } else if (pgn[i] === "{") {
-        ignore = true;
-      } else if (pgn[i + 1] === "." && pgn[i + 2] === ".") {
-        i += 4;
-        continue;
-      } else if (
-        i !== 0 &&
-        pgn[i] === "[" &&
-        pgn[i + 1] === "E" &&
-        pgn[i + 2] == "v"
-      ) {
-        break;
-      } else {
-        smit += pgn[i];
-      }
-    }
+    console.log(allGames);
+    let smit = allGames[0];
     console.log("Loading game", smit);
-    const temp = new Chess();
-    temp.load_pgn(smit);
-    setGame(temp);
-    game.load_pgn(smit);
-    setLoading(false);
   };
   useEffect(() => {
     loadGame();
