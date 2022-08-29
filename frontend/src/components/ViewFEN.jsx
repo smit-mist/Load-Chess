03d61ac732d17295a88d91e816ad9e4ced6de22a
smit-mist <smit3355mistry@gmail.com>
2022-08-29 08:46:02 +0530

    First game position loaded.

--- frontend/src/components/ViewFEN.jsx
+++ frontend/src/components/ViewFEN.jsx
@@ -6,12 +6,61 @@ import Loader from "./Loader";
 
 const ViewFEN = (props) => {
   const { allGames } = props;
-  const [game, setGame] = useState(new Chess());
   const [loading, setLoading] = useState(true);
+  const [game, setGame] = useState(new Chess());
   const loadGame = () => {
     console.log(allGames);
     let smit = allGames[0];
     console.log("Loading game", smit);
+    let curr = "";
+    let ignore = false;
+    let noStart = false;
+    for (let i = 0; i < smit.length; i++) {
+      if (ignore) {
+        if (smit[i] === "}") {
+          ignore = false;
+        } else {
+        }
+        continue;
+      } else if (smit[i] === "{") {
+        ignore = true;
+      } else if (noStart) {
+        if (smit[i] === "]") {
+          noStart = false;
+        } else {
+        }
+        continue;
+      } else if (smit[i] === "[") {
+        noStart = true;
+      } else {
+        curr += smit[i];
+      }
+    }
+
+    console.log("after proceccing", curr.trim());
+    let inFormat = "";
+    let ind = 0;
+    while (ind < curr.length) {
+      let temp = "";
+      while (ind < curr.length && curr[ind] !== " ") {
+        temp += curr[ind];
+        ind++;
+      }
+      if (temp.includes("...")) {
+        ind++;
+      } else {
+        inFormat += temp;
+        inFormat += " ";
+        ind++;
+      }
+
+      temp = "";
+    }
+    console.log(inFormat);
+    let newGame = new Chess();
+    newGame.load_pgn(inFormat);
+    setGame(newGame);
+    setLoading(false);
   };
   useEffect(() => {
     loadGame();
@@ -20,8 +69,9 @@ const ViewFEN = (props) => {
     return <Loader />;
   }
 
-  console.log("After loading", game.fen(), game.pgn());
-  return <Chessboard fen={game.fen()} animationDuration={500} />;
+  console.log("Before UI", game.fen());
+  console.log("PGN",  game.pgn());
+  return <Chessboard position={game.fen()} animationDuration={500} />;
 };
 
 export default ViewFEN;
