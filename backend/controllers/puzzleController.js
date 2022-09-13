8bb4a40cbf6efa3a7dd0ddce87620407ce0f6dee
smit-mist <smit3355mistry@gmail.com>
2022-09-13 16:01:50 +0530

    Tried to load puzzle csv (failed).

--- backend/controllers/puzzleController.js
+++ backend/controllers/puzzleController.js
@@ -1,6 +1,19 @@
+// var csv = require("node-csv").createParser();
+var csv = require("csv-parser");
+var data = [];
+var fs = require("fs");
 
- const getRandomPuzzle = (req, res) => {
+const getRandomPuzzle = (req, res) => {
+  fs.createReadStream("./backend/lichess_db_puzzle.csv")
+    .pipe(csv())
+    .on("data", function (row) {
+      data.push(row);
+    })
+    .on("end", function () {
+      console.log("Data loaded");
+    });
+  console.log(data.length);
   return res.status(200).json({ success: true });
 };
 
-module.exports = {getRandomPuzzle};
+module.exports = { getRandomPuzzle };
