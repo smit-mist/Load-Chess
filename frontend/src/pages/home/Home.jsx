c58e83ca6197bfae22159c48c776b3b87318ceea
smit-mist <smit3355mistry@gmail.com>
2022-09-15 10:00:02 +0530

    win loss chart added.

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Grid } from "@mui/material";
 import { useState } from "react";
 import { useEffect } from "react";
 import UserNameForm from "./UserNameForm";
 import ProfileTile from "./ProfileTile";
-
+import WinLoss from "./WinLoss";
 
 const Home = () => {
   const [profile, setProfile] = useState({});
@@ -13,16 +13,26 @@ const Home = () => {
     if (profile.id) setIsLoaded(true);
   }, [profile]);
   return (
-    <Grid container justifyContent="center">
-      <Grid item xs={3}>
-        <UserNameForm setProfile={setProfile} />
+    <Fragment>
+      {" "}
+      <Grid container justifyContent="center">
+        <Grid item xs={3}>
+          <UserNameForm setProfile={setProfile} />
+        </Grid>
+        {isLoaded && (
+          <Grid item xs={4}>
+            <ProfileTile profile={profile} />
+          </Grid>
+        )}
       </Grid>
       {isLoaded && (
-        <Grid item xs={4}>
-          <ProfileTile profile={profile} />
+        <Grid container>
+          <Grid item xs={4}>
+            <WinLoss profile={profile} />
+          </Grid>
         </Grid>
       )}
-    </Grid>
+    </Fragment>
   );
 };
 
---
+++ frontend/src/pages/home/WinLoss.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Paper } from "@mui/material";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const WinLoss = (props) => {
+  const { draw, win, loss } = props.profile.count;
+  console.log(draw, win, loss);
+
+  const data = {
+    labels: ["Win", "Loss", "Draw"],
+    datasets: [
+      {
+        label: "Game Results",
+        data: [win, loss, draw],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  return (
+    <Paper sx={{ margin: 2, padding: 3 }} elevation={17}>
+      <Pie data={data} redraw={true} />
+    </Paper>
+  );
+};
+
+export default WinLoss;
